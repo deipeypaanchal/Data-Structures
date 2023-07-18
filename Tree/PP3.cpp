d02df4e732a12f8bc7d35ac43ac9120356f03168
Deipey Paanchal <109501278+deipeypaanchal@users.noreply.github.com>
2023-07-18 10:18:39 -0400

    Update PP3.cpp

--- Tree/PP3.cpp
+++ Tree/PP3.cpp
@@ -1,4 +1,5 @@
-// Authors: Group25 (Brett and Deipey)
+// Author: Deipey Paanchal
+
 // This program is designed to implement a general tree to create decision trees using a linked structure
 // Reading data from the file to create the decision tree
 // Taking user input and printing the details of the prompted node
